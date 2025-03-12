4a9c4f62c4ce8c39c657714df30ab6527534a0ff
Shadrack Agyei Nti <116956047+ShadrackAgyei@users.noreply.github.com>
2025-03-12 02:10:54 +0000

    Update index.html
    adding sections for various teams

--- script.js
+++ script.js
@@ -1,13 +1,3 @@
 document.addEventListener('DOMContentLoaded', function() {
     console.log('iGEM 2025 homepage loaded');
 });
-function showDetails(name, role, bio) {
-    document.getElementById("modal-name").innerText = name;
-    document.getElementById("modal-role").innerText = role;
-    document.getElementById("modal-bio").innerText = bio;
-    document.getElementById("modal").style.display = "flex";
-}
-
-function closeModal() {
-    document.getElementById("modal").style.display = "none";
-}
